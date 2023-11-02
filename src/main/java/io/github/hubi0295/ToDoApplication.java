cc255bea264b5edca4ea45377599b52df4990cd6
= <nowa nazwa>
2023-11-02 11:58:02 +0100

    First Commit

---
+++ src/main/java/io/github/hubi0295/ToDoApplication.java
@@ -0,0 +1,13 @@
+package io.github.hubi0295;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoApplication.class, args);
+	}
+
+}
